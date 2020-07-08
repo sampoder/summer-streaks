ac8af15649064d7114298d09846a221d5baa78a8
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-07-08 13:29:08 +0800

    final stuff :)

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -27,12 +27,7 @@ fetch(url)
           "@" +
           data[i]["fields"]["Username"] +
           "</a>";
-        if (data[i]["fields"]["Max Streaks"] >= 8){
-          cell2.innerHTML = "🔥 " + "7+";
-        }
-        else{
-          cell2.innerHTML = "🔥 " + data[i]["fields"]["Max Streaks"];
-        } 
+        cell2.innerHTML = "🔥 " + data[i]["fields"]["Max Streaks"];
         cell2.style.borderLeftWidth = "0px";
       }
       if (i > rowNo - 1) {
